45cf284a6e71eaee09d94b1eb5b2f2c3482865bb
Nabeel Rasheed <nrasheed@accertify.com>
2014-09-03 13:35:19 -0500

    -

--- README.md
+++ README.md
@@ -1,2 +1,146 @@
 chidroid
 ========
+
+C.H.I.Droid
+
+limitFlags
+motorFlags
+otherFlags
+
+
+
+need commands to:
+
+  move up ^
+  move dn v
+  move lf >
+  move rt <
+  move in ;
+  move ot :
+  all stop ! or space
+  
+  neck servo ~
+  gripper servo #
+  gripper 
+  
+  
+  no-op query ?
+  
+  all commands return status flags
+
+move commands can specify no. of steps to move
+step size can be set (per axis?)
+
+set vars for:
+ gripper open limit
+ gripper close limit
+ 
+
+
+~
+`
+!
+@
+#
+$
+%
+^
+&
+*
+(
+)
+{
+}
+[
+]
+<
+>
+_
+-
++
+=
+|
+\
+/
+"
+'
+:
+;
+?
+.
+,
+
+
+check limit switches
+update limit flags
+
+update motor flags
+update motor pins
+
+check serial
+update command flags
+return flag statuses
+
+byte limitPins[] = {3,5,7,8};
+
+byte limitFlags = B10011110;
+
+for (byte i = 0; i < sizeof(limitPins); i++) {
+  
+}
+
+// serviceSerial checks the serial port and updates position with received data
+// it expects servo data in the form:
+// "180a" writes 180 to servo a
+// "90b writes 90 to servo b
+void serviceSerial()
+{
+static int pos = 0;
+if ( Serial.available()) {
+char ch = Serial.read();
+}
+}
+if(ch >= '0' && ch <= '9')
+// is ch a number?
+pos = pos * 10 + ch - '0';
+// yes, accumulate the value
+else if(ch >= 'a' && ch <= 'a'+ SERVOS) // is ch a letter for our servos?
+myservo[ch - 'a']
+
+https://www.newbiehack.com/MicrocontrollersConnectorsforPrototyping.aspx
+
+
+system has:
+  limitA0
+  limitA1
+  controlling motorA
+
+variables:
+  flagA0 0|1
+  flagA1 0|1
+  motorADir 0|1 (fwd|rev)
+  motorApwm 0-255
+
+loop:
+  if limitA0 tripped     set flagA0 = 1
+  if limitA0 not tripped set flagA0 = 0
+  if limitA1 tripped     set flagA1 = 1
+  if limitA1 not tripped set flagA1 = 0  
+
+
+http://www.google.com/patents/US4781305 (paper feeder patent)
+
+
+java facedetection
+http://cell0907.blogspot.com/2013/07/detecting-faces-in-your-webcam-stream.html
+
+java serial port
+http://www.lucid-control.com/how-to-access-serial-ports-in-java/
+http://playground.arduino.cc/interfacing/java
+
+
+
+JSSC:
+ downloaded https://java-simple-serial-connector.googlecode.com/files/jSSC-2.6.0-Release.zip
+ extracted jssc.jar from there
+ https://code.google.com/p/java-simple-serial-connector/wiki/jSSC_examples
--- src/java/org/nr/chidroid/Scratch1.java
+++ src/java/org/nr/chidroid/Scratch1.java
@@ -1,10 +1,96 @@
 package org.nr.chidroid;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import jssc.SerialPortList;
+
 public class Scratch1 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        //simplehttpserver();
+        getJsscPortNames();
         System.out.println("done");
+    }
+
+    public static void getJsscPortNames() {
+        String[] portNames = SerialPortList.getPortNames();
+        for (int i = 0; i < portNames.length; i++) {
+            System.out.println(portNames[i]);
+        }
+    }
+
+    public static void simplehttpserver() {
+        int port = 58080;
+        ServerSocket s = null;
+        while (true) {
+            try {
+                s = new ServerSocket(port);
+                Socket serverSocket = s.accept();
+                handleRequest(serverSocket);
+            } catch (IOException e) {
+                System.out.println("Failed to start server: " + e.getMessage());
+                System.exit(0);
+                return;
+            } finally {
+                if (null != s) {
+                    try {
+                        s.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void handleRequest(Socket s) {
+        BufferedReader is;     // inputStream from web browser
+        PrintWriter os;        // outputStream to web browser
+        String request;        // Request from web browser
+
+        try {
+            String webServerAddress = s.getInetAddress().toString();
+            System.out.println("Accepted connection from " + webServerAddress);
+            is = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            request = is.readLine();
+            System.out.println("Server recieved request from client: " + request);
+
+            os = new PrintWriter(s.getOutputStream(), true);
+            os.println("HTTP/1.0 200");
+            os.println("Content-type: text/html");
+            os.println("Server-name: myserver");
+            String response = "<html><head>" +
+                    "<title>Simpl Web Page</title></head>\n" +
+                    "<h1>Congratulations!!!</h1>\n" +
+                    "<h3>This page was returned by " + webServerAddress + "</h3>\n" +
+                    "<p>This is the first page hosted by your web server.\n</p>" +
+                    "Visit <A HREF=\"http://www.techwiki.ordak.org\"> http://www.techwiki.ordak.org</A> for more sample codes.\n" +
+                    "</html>\n";
+            os.println("Content-length: " + response.length());
+            os.println("");
+            os.println(response);
+            os.flush();
+            os.close();
+            s.close();
+        } catch (IOException e) {
+            System.out.println("Failed to send response to client: " + e.getMessage());
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return;
     }
 
 }
